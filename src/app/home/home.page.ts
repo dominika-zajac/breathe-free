7400a0f81313f80a3fa9f7941183773dc6cda1fa
Dominika Zajac <dominika.zajac@ge.com>
2019-12-03 18:51:08 +0100

    #4 - Replace mock data with data from BE

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,12 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  apiData: any;
+  isLoading: boolean;
+  level: string;
 
+  UNHEALTHY_LVL = 50;
+  DANGEROUS_LVL = 200;
 
 
   constructor(private http: HttpClient) {
@@ -15,18 +20,34 @@ export class HomePage {
   }
 
   reload() {
+    this.isLoading = true;
+    this.apiData = { data: {} };// explain how response looks like
     this.http
       .get(
         'https://api.waqi.info/feed/cracow/?token=62bdd9760e412e62ec5dd05d1d471b7a07c0e363'
       )
       .subscribe(
         response => {
-          console.log(response);
+          console.log(response) // see how response looks like
+          this.isLoading = false;
+          this.apiData = response;
+          this.level = this.aqiLevel();
         },
         error => {
           console.log(error);
+          this.isLoading = false;
         }
       );
+  }
+
+  private aqiLevel(): string {
+    if (!this.apiData || !this.apiData.data) {
+      return 'loading';
+    }
+    const aqi = this.apiData.data.aqi;
+    if (aqi < this.UNHEALTHY_LVL) return 'good';
+    if (aqi >= this.UNHEALTHY_LVL && aqi < this.DANGEROUS_LVL) return 'unhealthy';
+    else return 'dangerous';
   }
 
 }
